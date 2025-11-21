daf75efb9f66fd406ec5719d68e4dae30f1c57ac
Mir Ayan Ali <142529805+mirayanali5@users.noreply.github.com>
2025-11-21 15:38:45 +0530

    Update DES.java

--- DES.java
+++ DES.java
@@ -1,31 +1,43 @@
-import java.util.*
+import java.util.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
- 
-public class DES{
-public static void main(String args[]) throws Exception{
-Scanner sc = new Scanner(System.in);
- 
-System.out.print("Enter message to encrypt:");
-String msg = sc.nextLine();
-byte[] message = msg.getBytes();
- 
-System.out.print("Enter custom key:");
-String key = sc.nextLine();
-byte[]keyData = key.getBytes();
-DESKeySpec secretKey = new DESKeySpec(keyData);
- 
-SecretKeyFactory keyFactory = SecretKeyFactor.getInstance("DES");
-SecretKeyFactory keyN = KeyFactor.generateSecret(secretKey);
- 
-Cipher cipher = Cipher.getInstance("DES");
-cipher.init(Cipher.ENCRYPT_MODE, keyN);
-byte[] encrypted = cipher.doFinal(message);
- 
-cipher.init(Cipher.DECRYPT_MODE, keyN);
-byte[] decrypted = cipher.doFinal(encrypted);
-String decryptedMsg = new String(decrypted)
 
-System.out.println("Message:"+msg);
-System.out.println("Encrypted:"+encypted);
-System.out.println("Decrypted:"+decryptedMsg);
+public class DES {
+    public static void main(String args[]) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter message to encrypt: ");
+        String msg = sc.nextLine();
+        byte[] message = msg.getBytes();
+
+        System.out.print("Enter custom 8-byte key: ");
+        String key = sc.nextLine();
+
+        if (key.length() != 8) {
+            System.out.println("Error: DES key must be exactly 8 characters!");
+            return;
+        }
+
+        byte[] keyData = key.getBytes();
+        DESKeySpec secretKey = new DESKeySpec(keyData);
+
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey keyObj = keyFactory.generateSecret(secretKey);
+
+        Cipher cipher = Cipher.getInstance("DES");
+
+        cipher.init(Cipher.ENCRYPT_MODE, keyObj);
+        byte[] encrypted = cipher.doFinal(message);
+
+        cipher.init(Cipher.DECRYPT_MODE, keyObj);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        String decryptedMsg = new String(decrypted);
+
+        System.out.println("Message: " + msg);
+        System.out.println("Encrypted: " + Arrays.toString(encrypted));
+        System.out.println("Decrypted: " + decryptedMsg);
+
+        sc.close();
+    }
+}
